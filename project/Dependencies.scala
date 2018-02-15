7bbc6e95c6a2151e530310c319d54d64d168f75e
aebay <robochimp500@gmail.com>
2018-02-15 17:09:36 +0000

    Added test descriptions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,13 +1,15 @@
-
+import sbt._
 
 object Dependencies {
 
   // versions
+  lazy val scalaTestVersion = "3.0.4"
 
   // libraries
+  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
 
   // projects
   val commonDependencies =
-    Seq()
+    Seq( scalaTest )
 
 }
---
+++ src/test/scala/org/aeb/uk/carpark/LogicTest.scala
@@ -0,0 +1,21 @@
+package org.aeb.uk.carpark
+
+import org.scalatest.FlatSpec
+
+class LogicTest extends FlatSpec {
+
+  behavior of "car park"
+
+  it should "park a new car in an empty car park" is pending
+
+  it should "park a new car in the first empty slot in an occupied car park" is pending
+
+  it should "not park a new car in a full car park" is pending
+
+  it should "remove the most recently parked car" is pending
+
+  it should "be empty after the last car departs" is pending
+
+  it should "return the car with the smallest registration number" is pending
+
+}
